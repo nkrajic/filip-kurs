7ecbac039b8d5224fe28ebfa2d28c786d3d61c8f
nkrajic <nemanja.krajic@gmail.com>
2022-02-07 22:06:27 +0100

    fdgh

--- cypress/integration/POM/halloOglasi/mailinatorMessagePage.js
+++ cypress/integration/POM/halloOglasi/mailinatorMessagePage.js
@@ -1,19 +1,24 @@
 class mailinatorMessagePage{
     activationTextHalloOglasi = '[ng-repeat="email in emails"] > td:nth-of-type(3)'
-    messageBody = 'html_msg_body'
+    messageBody = '#html_msg_body'
 
     mailinatorActivationTextHalloOgasi(){
-        cy.get(this.activationTextHalloOglasi)
+        cy.get(this.activationTextHalloOglasi, {timeout: 60000})
         .click()
     }
 
     mailinatorMessageBody(){
         cy.get(this.messageBody,{timeout:10000})
-        .then( function($iframe){
-        const iFrameContent = $iframe.contents().find('div')
-        cy.wrap(iFrameContent)
-        .click()   
-        })
+        .should('exist')
+        .then(cy.wrap)
+        .find('[align="center"] strong')
+        .should('be.visible')
+        .click()
+        // // .then( function($iframe){
+        // // const iFrameContent = $iframe.contents().find('div')
+        // // cy.wrap(iFrameContent)
+        // .click()   
+        // })
     }
 
 }
